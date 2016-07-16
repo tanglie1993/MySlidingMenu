b0aef919244e60d2e53ea25d3bdba1ad10ef951a
tanglie1993 <tanglie23@163.com>
2016-07-16 12:32:35 +0800

    删除冗余的代码

--- app/src/main/java/tanglie/myapplication/MainActivity.java
+++ app/src/main/java/tanglie/myapplication/MainActivity.java
@@ -1,23 +1,11 @@
 package tanglie.myapplication;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 public class MainActivity extends SlidingFragmentActivity {
-
-    private Fragment mContent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,19 +13,9 @@ public class MainActivity extends SlidingFragmentActivity {
 
         setContentView(R.layout.responsive_content_frame);
 
-        if (findViewById(R.id.menu_frame) == null) {
-            setBehindContentView(R.layout.menu_frame);
-            getSlidingMenu().setSlidingEnabled(true);
-            getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-            // show home as up so we can toggle
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        } else {
-            // add a dummy view
-            View v = new View(this);
-            setBehindContentView(v);
-            getSlidingMenu().setSlidingEnabled(false);
-            getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-        }
+        setBehindContentView(R.layout.menu_frame);
+        getSlidingMenu().setSlidingEnabled(true);
+        getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
         SlidingMenu sm = getSlidingMenu();
         sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
@@ -45,32 +23,6 @@ public class MainActivity extends SlidingFragmentActivity {
         sm.setShadowDrawable(R.drawable.shadow);
         sm.setBehindScrollScale(0.25f);
         sm.setFadeDegree(0.25f);
-
-        // set the Above View Fragment
-        if (savedInstanceState != null)
-            mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
-        if (mContent == null)
-            mContent = new Fragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_frame, mContent)
-                .commit();
-
-        // set the Behind View Fragment
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.menu_frame, new Fragment())
-                .commit();
-
-
-    }
-
-
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        getSupportFragmentManager().putFragment(outState, "mContent", mContent);
     }
 
 }
