7232f4c88880838d4d445692b91188c97702d5bb
tanglie1993 <tanglie23@163.com>
2016-07-16 22:46:03 +0800

    注意：删除onmeasure方法后，上层的按钮会消失

--- app/src/main/java/tanglie/myapplication/slidingmenu/CustomViewAbove.java
+++ app/src/main/java/tanglie/myapplication/slidingmenu/CustomViewAbove.java
@@ -32,18 +32,18 @@ public class CustomViewAbove extends ViewGroup {
 	}
 
 
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-		int width = getDefaultSize(0, widthMeasureSpec);
-		int height = getDefaultSize(0, heightMeasureSpec);
-		setMeasuredDimension(width, height);
-
-		final int contentWidth = getChildMeasureSpec(widthMeasureSpec, 0, width);
-		final int contentHeight = getChildMeasureSpec(heightMeasureSpec, 0, height);
-		mContent.measure(contentWidth, contentHeight);
-	}
+//
+//	@Override
+//	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//
+//		int width = getDefaultSize(0, widthMeasureSpec);
+//		int height = getDefaultSize(0, heightMeasureSpec);
+//		setMeasuredDimension(width, height);
+//
+//		final int contentWidth = getChildMeasureSpec(widthMeasureSpec, 0, width);
+//		final int contentHeight = getChildMeasureSpec(heightMeasureSpec, 0, height);
+//		mContent.measure(contentWidth, contentHeight);
+//	}
 
 
 	@Override
