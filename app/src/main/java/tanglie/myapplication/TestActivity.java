17c7d1fe7e4c389fe174b47d037484c13afb9cf5
tanglie1993 <tanglie23@163.com>
2016-07-17 14:15:02 +0800

    使activity内容跟随手指移动

--- app/src/main/java/tanglie/myapplication/TestActivity.java
+++ app/src/main/java/tanglie/myapplication/TestActivity.java
@@ -4,10 +4,11 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
 
-import tanglie.myapplication.slidingmenu.SlidingMenu;
 import tanglie.myapplication.views.TestViewGroup;
 
 public class TestActivity extends AppCompatActivity {
@@ -33,12 +34,69 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                testViewGroup.getMenu().scrollTo(-100, 0);
-                testViewGroup.getContent().scrollTo(-200, 0);
-            }
-        }, 2000);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                testViewGroup.getMenu().scrollTo(-100, 0);
+//                testViewGroup.getContent().scrollTo(-200, 0);
+//            }
+//        }, 2000);
+    }
+
+    private VelocityTracker mVelocityTracker;
+
+
+    private int mPointerId;
+
+    private static final int PIXELS_PER_SECOND = 1000;
+    private static final int MAX_VELOCITY = 100000;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        addMovement(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //求第一个触点的id， 此时可能有多个触点，但至少一个
+                mPointerId = event.getPointerId(0);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                testViewGroup.getContent().scrollTo((int) -event.getX(), 0);
+                break;
+            case MotionEvent.ACTION_UP:
+                mVelocityTracker.computeCurrentVelocity(PIXELS_PER_SECOND, MAX_VELOCITY);
+                final float velocityX = mVelocityTracker.getXVelocity(mPointerId);
+                final float velocityY = mVelocityTracker.getYVelocity(mPointerId);
+                recordInfo(velocityX, velocityY);
+                releaseVelocityTracker();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                releaseVelocityTracker();
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void addMovement(final MotionEvent event) {
+        if(null == mVelocityTracker) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
+    }
+
+    private void releaseVelocityTracker() {
+        if(null != mVelocityTracker) {
+            mVelocityTracker.clear();
+            mVelocityTracker.recycle();
+            mVelocityTracker = null;
+        }
+    }
+
+    private static final String sFormatStr = "velocityX=%f\nvelocityY=%f";
+
+    private void recordInfo(final float velocityX, final float velocityY) {
+        System.out.println("recordInfo: velocityX " + velocityX + "velocityY " + velocityY);
+
     }
 }
